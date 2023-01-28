6b70d6df6a04e15d510269ffae233b084869b859
SkypeG <89580317+SkypeG@users.noreply.github.com>
2023-01-28 09:24:00 +0700

    Update @Install.bat

--- @Install.bat
+++ @Install.bat
@@ -1,5 +1,11 @@
 @echo off
 pip install pyautogui
 pip install colorama
+pip install phonenumbers
+pip install pycountry
+pip install opencage
+pip install folium
+pip install pyfiglet
+pip install tqdm
 
 pause
